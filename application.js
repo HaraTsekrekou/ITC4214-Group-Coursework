e06435bcef44620a07199cd63baa91f6a3bfb800
HaraTsekrekou <z.tsekrekou@acg.edu>
2021-10-20 21:41:09 +0300

    Added add row and delete row functions

--- application.js
+++ application.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
+    $(".add-row").click(function() {
+        var title = $("#title").val();
+        var artist = $("#artist").val();
+        var markup = "<tr><td><input type='checkbox' name='record'></td><td>" + title + "</td><td>" + artist + "</td></tr>";
+        $("table tbody").append(markup);
+    });
 
-
-
+    $(".delete-row").click(function() {
+        $("table tbody").find('input[name="record"]').each(function() {
+            if ($(this).is(":checked")) {
+                $(this).parents("tr").remove();
+            }
+        });
+    });
 });
